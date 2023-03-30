0af6d8681c1927f77858b6c8d7752ac3f64778e2
pravishti bhardwaj <bhard.pravi08@gmail.com>
2023-03-30 20:18:07 +0530

    changes done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,40 +1,46 @@
 import React from "react";
 import { addfavourites, Removefavourites } from "../actions";
 
-class MovieCard extends React.Component{
-    handleFavouriteClick=()=>{
-        const {movie} =this.props;
-        this.props.dispatch(addfavourites(movie))
-
-    }
-    handleUnFavouriteClick=()=>{
-        const {movie}=this.props;
-        this.props.dispatch(Removefavourites(movie))
-    }
-  render(){
-    const {movie,isFavourite} =this.props;
+class MovieCard extends React.Component {
+  handleFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(addfavourites(movie));
+  };
+  handleUnFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(Removefavourites(movie));
+  };
+  render() {
+    const { movie, isFavourite } = this.props;
     return (
-        
-        <div className="movie-card">
-            <div className="left">
-                <img src={movie.Poster} alt="poster1"></img>
-            </div>
-            <div className="right">
-                <div className="title">{movie.Title}</div>
-                <div className="plot">{movie.Plot}</div>
-                <div className="footer">
-                    <div className="rating">{movie.imdbRating}</div>
-                    {
-                        isFavourite?<button className="unfavourite-btn" onClick={this.handleUnFavouriteClick}>Unfavourite</button>:
-                        <button className="favourite-btn" onClick={this.handleFavouriteClick}>Favourite</button>
-                    }
-                    
-                </div>
-            </div>
-
-          
+      <div className="movie-card">
+        <div className="left">
+          <img src={movie.Poster} alt="poster1"></img>
         </div>
-      );
+        <div className="right">
+          <div className="title">{movie.Title}</div>
+          <div className="plot">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}</div>
+            {isFavourite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handleUnFavouriteClick}
+              >
+                Unfavourite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handleFavouriteClick}
+              >
+                Favourite
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
